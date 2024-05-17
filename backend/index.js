4024e29160137bf576aa9c14830874c6f84fe6c7
David2003Park <davidchanyoungpark@gmail.com>
2024-05-17 19:03:53 +1000

    change

--- backend/index.js
+++ backend/index.js
@@ -24,6 +24,7 @@ const connect = async () => {
     await mongoose.connect(process.env.MONGO_URI, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
+      useCreateIndex: true,
     });
 
     console.log("MongoDB database connected");
