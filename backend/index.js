479a86000f2371af32934613651ded644d0f9031
David2003Park <davidchanyoungpark@gmail.com>
2024-05-20 20:37:05 +1000

    update

--- backend/index.js
+++ backend/index.js
@@ -1,14 +1,14 @@
-const  express = require("express");
-const  dotenv = require("dotenv");
-const  mongoose = require("mongoose");
-const  cors = require("cors");
-const  cookieParser = require("cookie-parser");
-const  path = require("path");
-const  tourRoute = require("./routes/tours.js");
-const  userRoute = require("./routes/users.js");
-const  authRoute = require("./routes/auth.js");
-const  reviewRoute = require("./routes/reviews.js");
-const  bookingRoute = require("./routes/bookings.js");
+const express = require("express");
+const dotenv = require("dotenv");
+const mongoose = require("mongoose");
+const cors = require("cors");
+const cookieParser = require("cookie-parser");
+const path = require("path");
+const tourRoute = require("./routes/tours.js");
+const userRoute = require("./routes/users.js");
+const authRoute = require("./routes/auth.js");
+const reviewRoute = require("./routes/reviews.js");
+const bookingRoute = require("./routes/bookings.js");
 
 dotenv.config();
 const app = express();
@@ -17,18 +17,16 @@ const corsOptions = {
   origin: "*",
 };
 
-// Log environment variable
-mongoose.connect(process.env.MONGO_URI || 'mongodb+srv://davidpark:merntourbooking@cluster0.ycdvuuw.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0');
-console.log('MONGO_URI:', process.env.MONGO_URI);
+// Log environment variables
+console.log('Environment Variables:', process.env);
 
-// database connection
+// Database connection
 mongoose.set("strictQuery", false);
 const connect = async () => {
   try {
     await mongoose.connect(process.env.MONGO_URI, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
-      useCreateIndex: true,
     });
     console.log("MongoDB database connected");
   } catch (err) {
@@ -36,7 +34,9 @@ const connect = async () => {
   }
 };
 
-// middleware
+connect();
+
+// Middleware
 app.use(express.json());
 app.use(cors(corsOptions));
 app.use(cookieParser());
@@ -45,11 +45,20 @@ app.use("/api/v1/tours", tourRoute);
 app.use("/api/v1/users", userRoute);
 app.use("/api/v1/review", reviewRoute);
 app.use("/api/v1/booking", bookingRoute);
+
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../frontend/build')));
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, '../frontend/build', 'index.html'));
+  });
 }
 
 app.listen(port, () => {
-  connect();
   console.log("Server listening on port", port);
+});
+
+// Error handling middleware
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).send('Something broke!');
 });
